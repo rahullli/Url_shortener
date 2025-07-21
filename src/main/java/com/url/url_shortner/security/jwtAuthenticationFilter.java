5175dc81d00c47c5cd91f7ed10f39bf4cabe4d5d
Rahul Khandelwal <rahul.khandelwal@soti.net>
2025-07-21 23:54:08 +0530

    Added jwt token validation.

---
+++ src/main/java/com/url/url_shortner/security/jwtAuthenticationFilter.java
@@ -0,0 +1,42 @@
+package com.url.url_shortner.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+// It will make sure every request will have the JWT auth token in it.
+public class jwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private jwtUtils jwtTokenProvider;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try{
+//            Extract the JWT token from the header.
+            String jwt = jwtTokenProvider.getJWTTokenFromBearerToken(request);
+//            Validate token
+            if(jwt != null && jwtTokenProvider.validateToken(jwt)){
+                String userName = jwtTokenProvider.getUserNameFromJwtToken(jwt);
+//                so here the userDetailsService is from the string security , so I have to create the implementation class
+//                for it inorder to return the user data in the format which I have defined.
+                UserDetails userDetails = userDetailsService.loadUserByUsername(userName);
+            }
+//            If valid , Get user details
+//            Get use Name -> Load User -> Set the auth Context.
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/com/url/url_shortner/security/jwtAuthenticationResponse.java
@@ -0,0 +1,9 @@
+package com.url.url_shortner.security;
+
+import lombok.Data;
+
+@Data
+public class jwtAuthenticationResponse {
+
+
+}
